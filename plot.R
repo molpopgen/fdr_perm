0227066c1c8b9fc867132e438ba8880eaaecdb82
molpopgen <krthornt@uci.edu>
2017-02-15 20:12:53 -0800

    touch up the plot

--- plot.R
+++ plot.R
@@ -3,6 +3,6 @@ library(reshape)
 x=read.table("mean_fdr.txt",header=T)
 xr=melt(x,id=c("upreg","tpr","noise"))
 print(head(xr))
-p = ggplot(xr,aes(tpr,value)) + geom_line(aes(linetype=variable)) + facet_wrap(upreg~noise) 
+p = ggplot(xr,aes(tpr,value)) + geom_line(aes(color=variable,linetype=variable)) + facet_wrap(upreg~noise) + theme_bw() + xlab("True positive rate") + ylab("False discovery rate") + theme(axis.text.x=element_text(angle=75,hjust=1))
 
 ggsave("fdr.pdf",p)
